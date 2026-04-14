fe8b50932c32464ea48b3551415817a178bd4f11
Matheus Viturino <65409312+Matheus-MMVIII@users.noreply.github.com>
2026-04-14 22:34:02 +0000

    Create base App Config.

---
+++ src/main/java/com/basic/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.basic.config;
+
+public final class AppConfig {
+    private static final String PORT = "8080";
+
+    AppConfig() {
+
+    }
+
+    public String getPort() {
+        return PORT;
+    }
+}
