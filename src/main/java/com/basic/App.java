d422b73bfe3eae0e1e6bcacf500f14c058729dc1
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-04-15 19:50:46 -0300

    Create Api and open port in 8080.

--- src/main/java/com/basic/App.java
+++ src/main/java/com/basic/App.java
@@ -1,8 +1,13 @@
 package com.basic;
 
+import java.io.IOException;
+
+import com.basic.http.ApiServer;
+
 public class App {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        ApiServer server = new ApiServer();
+        server.start();
     }
 }
---
+++ src/main/java/com/basic/http/ApiServer.java
@@ -0,0 +1,37 @@
+package com.basic.http;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.basic.config.AppConfig;
+import com.basic.http.handler.HealthHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class ApiServer {
+    private final HttpServer server;
+    private final ExecutorService executor;
+
+    public ApiServer() throws IOException {
+        this.server = HttpServer.create(new InetSocketAddress(AppConfig.getPort()), AppConfig.getBacklog());
+        this.executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        registerContexts();
+        server.setExecutor(executor);
+        Runtime.getRuntime().addShutdownHook(new Thread(this::stop));
+    }
+
+    public void start() {
+        server.start();
+        System.out.println("Servidor iniciado na porta " + AppConfig.getPort());
+    }
+
+    public void stop() {
+        server.stop(0);
+        executor.shutdown();
+    }
+
+    private void registerContexts() {
+        server.createContext("/health", new HealthHandler());
+    }
+}
