7b3ee61e891a77671b5e3f5cfc63b7ed9c4245b9
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-05-03 21:55:54 -0300

    Simplified App class.

--- src/main/java/com/basic/App.java
+++ src/main/java/com/basic/App.java
@@ -1,7 +1,6 @@
 package com.basic;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 import com.basic.http.ApiServer;
 
@@ -9,14 +8,6 @@ public class App {
 
   public static void main(String[] args) throws IOException {
     ApiServer server = new ApiServer();
-    Scanner sc = new Scanner(System.in);
     server.start();
-
-    do {
-      System.out.println("Digite STOP para desligar o servidor. ");
-    } while (!sc.next().equalsIgnoreCase("Stop"));
-    server.stop();
-    sc.close();
-    System.out.println("Servidor desligado. ");
   }
 }
